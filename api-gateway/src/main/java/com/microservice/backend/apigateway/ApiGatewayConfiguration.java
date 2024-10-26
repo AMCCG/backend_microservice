979506d2d3bfeab2fa71cf1c87701a63ab8d82d4
Apisit <amornchanchaigul@gmaul.com>
2024-10-26 15:38:18 +0700

    update api gateway

--- api-gateway/src/main/java/com/microservice/backend/apigateway/ApiGatewayConfiguration.java
+++ api-gateway/src/main/java/com/microservice/backend/apigateway/ApiGatewayConfiguration.java
@@ -11,7 +11,7 @@ public class ApiGatewayConfiguration {
     @Bean
     public RouteLocator gatewayRouter(RouteLocatorBuilder routeLocatorBuilder) {
         return routeLocatorBuilder.routes()
-                .route(p -> p.path("/hello-world/**").uri("lb://hello-world"))
+                .route(p -> p.path("/hello-world/**").uri("http://127.0.0.1:8001/hello-world/"))
                 .route(p -> p.path("/python-backend-service/**").uri("http://127.0.0.1:8000/python-backend-service/")
                 ).build();
     }
